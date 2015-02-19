b4b18a12cf2dcdf5ceebdacc620a7d71da5e0b0e
Shervin Shaikh <Shervin.Shaikh@citrix.com>
2015-02-19 15:13:50 -0600

    Grab roomID from the user

--- Chat/ViewController.swift
+++ Chat/ViewController.swift
@@ -9,17 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var roomID: UITextField!
+    @IBOutlet weak var roomIdLabel: UILabel!
+    @IBOutlet weak var joinRoom: UIButton!
+    
+//    var ref: Firebase!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+//        ref = Firebase(url:"http://chat.firebaseio.com/shervinRoom")
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        title = "Shervin Chat"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    @IBAction func joinRoom(sender: AnyObject) {
+        roomIdLabel.text = roomID.text
+    }
 }
 
